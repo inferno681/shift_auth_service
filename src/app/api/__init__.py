c230ed453888442926fed43cbf05b0192f5dbc5b
Vasilii Stakrotckii <inferno681@gmail.com>
2024-08-08 16:49:45 +0200

    fiX

--- src/app/api/__init__.py
+++ src/app/api/__init__.py
@@ -6,12 +6,12 @@ from .endpoints import router_auth, router_check, router_healthz
 
 router = APIRouter()
 router.include_router(
-    router_auth, tags=[config.service.tags_metadata_auth['name']]
+    router_auth, tags=[config.service.tags_metadata_auth['name']]  # type: ignore
 )
 router.include_router(
-    router_check, tags=[config.service.tags_metadata_check['name']]
+    router_check, tags=[config.service.tags_metadata_check['name']]  # type: ignore
 )
 
 router.include_router(
-    router_healthz, tags=[config.service.tags_metadata_health['name']]
+    router_healthz, tags=[config.service.tags_metadata_health['name']]  # type: ignore
 )
