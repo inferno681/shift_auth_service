278d42678d57e6c7e12b133bd0b70f7aba3b148c
Vasilii Stakrotckii <inferno681@gmail.com>
2024-08-17 02:59:47 +0200

    fix

--- src/app/db/database.py
+++ src/app/db/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from config import config
 
 engine = create_async_engine(
-    url=config.database_url,
-    echo=config.service.db_echo,
+    url=config.database_url,  # type: ignore
+    echo=config.service.db_echo,  # type: ignore
 )
 
 async_session = async_sessionmaker(bind=engine)
